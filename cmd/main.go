cbea8d19319e372df091efef1735a8513ad7718b
preegnees <mrraziapov13@gmail.com>
2022-11-19 01:32:19 +0300

    все закомментировал

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"context"
+	// "context"
 
-	hs "jwt/pkg/handlers"
-	sr "jwt/pkg/server"
-	ss "jwt/pkg/services"
-	se "jwt/pkg/storage"
+	// hs "jwt/pkg/handlers"
+	// sr "jwt/pkg/server"
+	// ss "jwt/pkg/services"
+	// se "jwt/pkg/storage"
 )
 
 func main() {
 
-	var storage se.IStorage = se.New()
-	var services ss.IService = ss.New(storage)
-	var handlers hs.IHandlers = hs.New(services)
+	// var storage se.IStorage = se.New()
+	// var services ss.IService = ss.New(storage)
+	// var handlers hs.IHandlers = hs.New(services)
 
-	ctx := context.Background()
-	var server sr.IServer = sr.New(ctx, "localhost:80", handlers)
-	if err := server.Run(); err != nil {
-		panic(err)
-	}
+	// ctx := context.Background()
+	// var server sr.IServer = sr.New(ctx, "localhost:80", handlers)
+	// if err := server.Run(); err != nil {
+	// 	panic(err)
+	// }
 }
