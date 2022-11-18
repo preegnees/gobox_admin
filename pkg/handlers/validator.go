87ae087b86d042479bbb2e4ba2d2bb18847cb20e
preegnees <mrraziapov13@gmail.com>
2022-11-19 01:39:42 +0300

    вынес валидатор

---
+++ pkg/handlers/validator.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"github.com/go-playground/validator"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
