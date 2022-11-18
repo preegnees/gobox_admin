22d61115d15d68551c9ebeb6db62ff7e5ec68c94
preegnees <mrraziapov13@gmail.com>
2022-11-19 01:41:08 +0300

    тестирование jwt

--- pkg/utils/jwt/jwt_test.go
+++ pkg/utils/jwt/jwt_test.go
@@ -1,29 +1,38 @@
 package jwt
 
 import (
+	"os"
 	"testing"
 )
 
 const SECREST = "test"
 const USERNAME = "my_name"
 
+var jwt_ IJWT = nil 
+
+func TestMain(m *testing.M) {
+	os.Setenv("SECRET", SECREST)
+	jwt_, _ = New()
+	m.Run()
+}
+
 func TestGenJWT(t *testing.T) {
-	aToken, rToken, err := GenerateJWT(SECREST, USERNAME, User)
+	aToken, rToken, err := jwt_.GenerateJWT(USERNAME, User)
 	if err != nil || aToken == "" || rToken == "" {
 		t.Fatal(err)
 	}
 }
 
 func TestCheckJWT(t *testing.T) {
-	b := CheckJwt(SECREST, "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2Njg3OTI3NjgsImV4cCI6NDg1NjAwMjM2OCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.5yE8QDPshNnVCR5GX3HT6_TtpNd2KsU6ywpf_MQUDQg")
+	b := jwt_.CheckJwt("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2Njg3OTI3NjgsImV4cCI6NDg1NjAwMjM2OCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.5yE8QDPshNnVCR5GX3HT6_TtpNd2KsU6ywpf_MQUDQg")
 	if !b {
 		t.Fatal("!b")
 	}
 }
 
 func TestGetValues(t *testing.T) {
-	aToken, _, _ := GenerateJWT(SECREST, USERNAME, User)
-	myCl, err := GetValuesFromJWT(SECREST, aToken)
+	aToken, _, _ := jwt_.GenerateJWT(USERNAME, User)
+	myCl, err := jwt_.GetValuesFromJWT(aToken)
 	if err != nil {
 		t.Fatal(err)
 	}
