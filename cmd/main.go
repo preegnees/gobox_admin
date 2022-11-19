b8bf72d5e0734999e8a439fcb7dddcae6a522a0d
preegnees <mrraziapov13@gmail.com>
2022-11-19 13:16:42 +0300

    main

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
-	// "context"
+	"context"
+	"os"
 
-	// hs "jwt/pkg/handlers"
-	// sr "jwt/pkg/server"
-	// ss "jwt/pkg/services"
-	// se "jwt/pkg/storage"
+	handlers "jwt/pkg/handlers"
+	middlewares "jwt/pkg/middlewares"
+	server "jwt/pkg/server"
+	services "jwt/pkg/services"
+	storage "jwt/pkg/storage"
+	jwt "jwt/pkg/utils/jwt"
 )
 
 func main() {
+	os.Setenv("SECRET", "TEST")
+	jwt_, err := jwt.New()
+	if err != nil {
+		panic(err)
+	}
+	var storage_ storage.IStorage = storage.New()
+	var services_ services.IService = services.New(storage_)
+	var handlers_ handlers.IHandlers = handlers.New(services_, jwt_)
+	var middlewares_ middlewares.IMiddleware = middlewares.New(jwt_)
 
-	// var storage se.IStorage = se.New()
-	// var services ss.IService = ss.New(storage)
-	// var handlers hs.IHandlers = hs.New(services)
-
-	// ctx := context.Background()
-	// var server sr.IServer = sr.New(ctx, "localhost:80", handlers)
-	// if err := server.Run(); err != nil {
-	// 	panic(err)
-	// }
+	ctx := context.Background()
+	var server server.IServer = server.New(ctx, "localhost:80", handlers_, middlewares_)
+	if err := server.Run(); err != nil {
+		panic(err)
+	}
 }
