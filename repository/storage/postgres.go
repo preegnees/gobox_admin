2abbfa89e7bb7cce1f32c6ab65db2e8f1a3926ab
preegnees <mrraziapov13@gmail.com>
2022-11-24 01:51:01 +0300

    написал методы сохарения пользователя, получение пользователя, сохранение, удаление токена

--- repository/storage/postgres.go
+++ repository/storage/postgres.go
@@ -1,10 +1,10 @@
 package storage
 
 import (
-	dto "core/dto"
-	// models "core/models"
 	"context"
+	dto "core/dto"
 	repository "core/repository"
+	models "core/repository/models"
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
@@ -23,9 +23,10 @@ type ConfPostgres struct {
 	Host     string
 	Port     int
 	DB       string
+	Sslmode  string
 }
 
-func New(cnf *ConfPostgres) (*postrges_, error){
+func New(cnf *ConfPostgres) (*postrges_, error) {
 	if cnf == nil {
 		cnf = &ConfPostgres{
 			Cxt:      context.TODO(),
@@ -38,12 +39,13 @@ func New(cnf *ConfPostgres) (*postrges_, error){
 	}
 
 	url := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", (*cnf).User, (*cnf).Password, (*cnf).Host, (*cnf).Port, (*cnf).DB)
+	println(url)
 	conn, err := pgx.Connect((*cnf).Cxt, url)
 	if err != nil {
 		return nil, err
 	}
 	if err := conn.Ping((*cnf).Cxt); err != nil {
-		return nil, nil
+		return nil, err
 	}
 
 	return &postrges_{
@@ -53,23 +55,88 @@ func New(cnf *ConfPostgres) (*postrges_, error){
 
 func (p *postrges_) SaveUser(ctx context.Context, user *dto.DTOUser) (err error) {
 
-	query := 
+	query :=
+		`
+	INSERT INTO users (username, password_hash, user_role, email) 
+	VALUES ($1, $2, $3, $4)
 	`
-	INSERT INTO users (username, password) 
-	VALUES ($1, $2)
-	`
-	_, err = p.conn.Exec(ctx, query, user.Username, user.Password)
+	_, err = p.conn.Exec(ctx, query, user.Username, user.PasswordHash, user.UserRole, user.Email)
 	if err != nil {
 		return err
 	}
-	return nil 
+	return nil
 }
-func (p *postrges_) SaveToken(ctx context.Context, token *dto.DTOTokens) (err error) {
-	return nil 
+
+func (p *postrges_) FindUserByUsername(ctx context.Context, username string) (*dto.DTOUser, error) {
+	query :=
+		`
+	SELECT user_id, username, password_hash, user_role, email 
+	FROM users 
+	WHERE username=$1
+	`
+	user := models.User{}
+	err := p.conn.QueryRow(ctx, query, username).Scan(&user.UserId, &user.Username, &user.PasswordHash, &user.UserRole, &user.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.DTOUser{
+		Username:     user.Username,
+		PasswordHash: user.PasswordHash,
+		UserRole:     user.UserRole,
+		Email:        user.Email,
+	}, nil
 }
-func (p *postrges_) RemoveToken(ctx context.Context, token *dto.DTOTokens) (err error) {
-	return nil 
+
+func (p *postrges_) ChangeRoleUser(ctx context.Context, username string, role string) error {
+	return nil
+}
+
+func (p *postrges_) ChangeEmailUser(ctx context.Context, username string, role string) error {
+	return nil
+}
+
+func (p *postrges_) SaveToken(ctx context.Context, token *dto.DTOTokens) error {
+	query :=
+		`
+		SELECT user_id 
+		FROM users 
+		WHERE username=$1
+	`
+	user_id := -1
+	err := p.conn.QueryRow(ctx, query, token.Username).Scan(&user_id)
+	if err != nil {
+		return err
+	}
+
+	if user_id != -1 {
+		query =
+			`
+		INSERT INTO tokens (temp_token, user_id)
+		VALUES ($1, $2)
+		`
+		_, err = p.conn.Exec(ctx, query, token.Token, user_id)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func (p *postrges_) RemoveToken(ctx context.Context, token *dto.DTOTokens) (error) { // сюда нужно передавать DTO STORAGE
+
+	query :=
+		`
+	DELETE 
+	FROM tokens 
+	WHERE temp_token=$1
+`
+	_, err := p.conn.Exec(ctx, query, token.Token)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 func (p *postrges_) GetTokens(ctx context.Context, username string) (tokens *[]dto.DTOTokens, err error) {
-	return nil, nil 
+	return nil, nil
 }
--- repository/storage/postgres_test.go
+++ repository/storage/postgres_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestSaveUser(t *testing.T) {
 	c := ConfPostgres{
-		Cxt: context.TODO(),
-		User:     "postgres",
-		Password: "postgres",
+		Cxt:      context.TODO(),
+		User:     "docker",
+		Password: "docker",
 		Host:     "localhost",
 		Port:     5431,
-		DB:       "postgres",
+		DB:       "docker",
 	}
 	p, err := New(&c)
 	if err != nil {
@@ -21,10 +21,89 @@ func TestSaveUser(t *testing.T) {
 	}
 	ctx := context.TODO()
 	user := dto.DTOUser{
-		Username: "username",
-		Password: "pass",
+		Username:     "username",
+		PasswordHash: "pass",
+		UserRole: "u",
+		Email: "email@",
 	}
 	if err := p.SaveUser(ctx, &user); err != nil {
 		panic(err)
 	}
 }
+
+func TestFindUserByUsername(t *testing.T) {
+	c := ConfPostgres{
+		Cxt:      context.TODO(),
+		User:     "docker",
+		Password: "docker",
+		Host:     "localhost",
+		Port:     5431,
+		DB:       "docker",
+	}
+	p, err := New(&c)
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.TODO()
+	username := "roman"
+	user, err := p.FindUserByUsername(ctx, username)
+	if err != nil {
+		panic(err)
+	}
+	if (*user).Username != username {
+		panic("(*user).Username != username")
+	}
+}
+
+func TestSaveToken(t *testing.T) {
+	c := ConfPostgres{
+		Cxt:      context.TODO(),
+		User:     "docker",
+		Password: "docker",
+		Host:     "localhost",
+		Port:     5431,
+		DB:       "docker",
+	}
+	p, err := New(&c)
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.TODO()
+	token := dto.DTOTokens{
+		Username: "roman",
+		Token: "token1",
+	}
+	err = p.SaveToken(ctx, &token)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestRemoveToken(t *testing.T) {
+	c := ConfPostgres{
+		Cxt:      context.TODO(),
+		User:     "docker",
+		Password: "docker",
+		Host:     "localhost",
+		Port:     5431,
+		DB:       "docker",
+	}
+	p, err := New(&c)
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.TODO()
+	token := dto.DTOTokens{
+		Username: "roman",
+		Token: "token1",
+	}
+	err = p.SaveToken(ctx, &token)
+	if err != nil {
+		
+	}
+	err = p.RemoveToken(ctx, &token)
+	if err != nil {
+		panic(err)
+	}
+}
+
